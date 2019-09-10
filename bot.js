19ce37d130b487baf978c809fd1b85b57d8a0ddd
Ciaran Ruane <ciruane@tcd.ie>
2019-09-10 16:16:23 +0100

    add matchmaking

--- bot.js
+++ bot.js
@@ -8,6 +8,9 @@ const signUpTime = 18;
 const gameTimes = [2, 500]; // minutes from signup time to team announcement
 var announcementsChannelID = 608298295202414595;
 
+// The amount of MMR lower someone should be considered if they're on a secondary role/autofilled
+const secondariesPenalty = 5;
+const autofillsPenalty = 20;
 
 // Class to represent a player
 class Player 
@@ -24,6 +27,71 @@ class Player
         this.gamesMissed = gamesMissed;
         this.namePadded = nameDisplay.padEnd(30, ' ');
     }
+
+    getMMR()
+    {
+        return this.mmr;
+    }
+}
+
+class Team
+{
+    constructor()
+    {
+        var top = 0;
+        var jung = 0;
+        var mid = 0;
+        var adc = 0;
+        var supp = 0;
+        var secondaries = 0;
+        var autofills = 0;
+    }
+
+    getMMR()
+    {
+        var total = this.top.mmr + this.jung.mmr + this.mid.mmr + this.adc.mmr + this.supp.mmr;
+        return ((total/5) - (this.secondaries*secondariesPenalty + this.autofills*autofillsPenalty));
+    }
+
+    fillTeam(player)
+    {
+        var placed = false;
+        if(!this.top)
+        {
+            this.top = player;
+            placed = true;
+        }
+        else if(!this.jung)
+        {
+            this.jung = player;
+            placed = true;
+        }
+        else if(!this.mid)
+        {
+            this.mid = player;
+            placed = true;
+        }
+        else if(!this.adc)
+        {
+            this.adc = player;
+            placed = true;
+        }
+        else if(!this.supp)
+        {
+            this.supp = player;
+            placed = true;
+        }
+
+        if(placed)
+        {
+            if(player.rolePrimary != 'F' && player.roleSecondary != 'F')
+                this.autofills++;
+            else if(player.rolePrimary != 'F')
+                this.secondaries++;
+            return 1;
+        }
+        return 0;
+    }
 }
 
 class Match
@@ -32,29 +100,14 @@ class Match
     {
         this.blueTeam = blueTeam;
         this.redTeam = redTeam;
-        this.blueMMR = (blueTeam[0].mmr + blueTeam[1].mmr + blueTeam[2].mmr + blueTeam[3].mmr + blueTeam[4].mmr)/5;
-        this.redMMR = (redTeam[0].mmr + redTeam[1].mmr + redTeam[2].mmr + redTeam[3].mmr + redTeam[4].mmr)/5;
     }
 
     teamsString()
     {
-        var str = "BLUE TEAM: "
-        for(var i = 0; i < 5; i++)
-        {
-            if(i<4)
-                str += this.blueTeam[i].nameDisplay + ", ";
-            else
-                str += this.blueTeam[i].nameDisplay;
-        }
-        str += "\nRED TEAM: "
-        for(var i = 0; i < 5; i++)
-        {
-            if(i<4)
-                str += this.redTeam[i].nameDisplay + ", ";
-            else
-                str += this.redTeam[i].nameDisplay;
-        }
-        console.log(str);
+        var str = "BLUE TEAM: " + this.blueTeam.top.nameDisplay + ", " + this.blueTeam.jung.nameDisplay + ", " 
+                + this.blueTeam.mid.nameDisplay + ", " + this.blueTeam.adc.nameDisplay + ", " + this.blueTeam.supp.nameDisplay;
+        str += "\nRED TEAM: " + this.redTeam.top.nameDisplay + ", " + this.redTeam.jung.nameDisplay + ", " 
+                + this.redTeam.mid.nameDisplay + ", " + this.redTeam.adc.nameDisplay + ", " + this.redTeam.supp.nameDisplay;
         return str;
     }
 }
@@ -138,8 +191,8 @@ function readPlayerList()
     fs.readFile('players.txt', 'utf8', (err, fd) => {
         if (err) {
             if (err.code === 'ENOENT') {
-            console.error('players.txt does not exist in this directory');
-            return;
+                console.error('players.txt does not exist in this directory');
+                return;
             }
         
             throw err;
@@ -190,65 +243,210 @@ async function buildMatch(message, emoji)
     // maybe I shouldn't be using this as an array, it was a collection before (which extends map)
     // maybe this would all be better as a collection.
     var reactions = message.reactions;
-    var matchPlayerList = [];
+    var roundPlayerList = [];
 
-    reactions.forEach((element) => addPlayerToMatch(element, matchPlayerList, emoji));
-    matchPlayerList.forEach(element => console.log(element.nameDiscord));
-    console.log(matchPlayerList.length);
-    if((matchPlayerList.length % 10) >= 8)
+    reactions.forEach((element) => addPlayerToRound(element, roundPlayerList, emoji));
+    
+    if((roundPlayerList.length % 10) >= 8)
     {
-        announcementsChannel.send("Almost enough players for an extra game (need " + (10-(matchPlayerList.length % 10)) + "), waiting another 5 minutes to start");
+        announcementsChannel.send("Almost enough players for an extra game (need " + (10-(roundPlayerList.length % 10)) + "), waiting another 5 minutes to start");
         await setTimeout(() => {}, 5*60*1000);
+        var roundPlayerList = [];
+        reactions.forEach((element) => addPlayerToRound(element, roundPlayerList, emoji));
     }
     for(var i = 0; i < playerList.length; i++)
-        matchPlayerList.push(playerList[i]); // This is just for testing, since I can't react to the message 30 times
+        roundPlayerList.push(playerList[i]); // This is just for testing, since I can't react to the message 30 times
 
-    shuffle(matchPlayerList);
-
-    var numOfGames = Math.floor(matchPlayerList.length / 10);
-    for(var i = 0; i < matchPlayerList.length; i++)
+    var numOfGames = Math.floor(roundPlayerList.length / 10);
+    roundPlayerList.sort(byGamesMissed);
+    for(var i = roundPlayerList.length - 1; i >= numOfGames * 10; i--)
     {
-        //console.log(matchPlayerList[i].nameDisplay);
+        roundPlayerList[i].gamesMissed++;
+        roundPlayerList.pop();
     }
+
+    if(roundPlayerList.length % 10 != 0)
+        console.error( roundPlayerList.length + "SOMETHING WENT HORRIBLY WRONG");
+
+    shuffle(roundPlayerList);
+    const numOfTeams = numOfGames * 2;
+    var teams = [];
+    for(var i = 0; i < numOfTeams; i++)
+    {
+        var temp = new Team();
+        temp.autofills = 0;
+        temp.secondaries = 0;
+        await teams.push(temp);
+    }
+    matchmake(roundPlayerList, teams);
+
+    teams.sort(byMMR);
+
     announcementsChannel.send("We have numbers for " + (numOfGames) + " games!");
     for(var i = 0; i < numOfGames; i++)
     {
-        var gameMessage = "GAME " + (i+1) + ":\n";
-        var blueTeam = [matchPlayerList[(i*10)], matchPlayerList[(i*10) + 1], matchPlayerList[(i*10) + 2], matchPlayerList[(i*10) + 3], matchPlayerList[(i*10) + 4]];
-        var redTeam = [matchPlayerList[(i*10) + 5], matchPlayerList[(i*10) + 6], matchPlayerList[(i*10) + 7], matchPlayerList[(i*10) + 8], matchPlayerList[(i*10) + 9]];
-        console.log(blueTeam);
-        var thisGame = new Match(blueTeam, redTeam);
+        var gameMessage = "GAME " + (i+1) + ": " + teams[i*2].getMMR() + " MMR vs " + teams[(i*2)+1].getMMR() + " MMR \n";
+        //console.log(teams[i*2] + " \n " + teams[(i*2)+1])
+        var thisGame = new Match(teams[i*2], teams[(i*2)+1]);
         await announcementsChannel.send(gameMessage + thisGame.teamsString() + "\n\n\n");
     }
-    
-    /*for(var i = 0; i < reactions.length; i++)
-    {
-        if(reactions[i]._emoji.name == emoji)
-        {
-            var usersForThisMatchTime = reactions[i].users.array();
-            for(var j = 0; j < usersForThisMatchTime.length; j++)
-            {
-                var matchPlayerList.push
-                announcementsChannel.send(usersForThisMatchTime[j].username);
-            }
-        }
-    }
-    announcementsChannel.send("we the bois");*/
 }
 
-function addPlayerToMatch(thisReaction, list, thisGame)
+function addPlayerToRound(thisReaction, list, thisGame)
 {
     if(thisReaction._emoji.name == thisGame)
     {
         thisReaction.users.forEach(element => {
             var playerIndex = playerListContains(playerList, element.username);
-            console.log(playerIndex);
             if(playerIndex != -1)
             {
                 list.push(playerList[playerIndex]);
             }
         });
     }
+}
+/*  
+    ----------------------------------------MATCHMAKING FUNCTIONS-------------------------------------------
+    Anything to do with matchmaking goes here 
+    --------------------------------------------------------------------------------------------------------
+*/
+
+function matchmake(listOfPlayers, teams)
+{
+    var unassignedPlayers = listOfPlayers.length;
+    var fillPlayers = 0;
+    while(unassignedPlayers > 0)
+    {
+        var currentPlayer = listOfPlayers[unassignedPlayers-1];
+        if(fillPlayers < unassignedPlayers)
+        {
+            var success = 0
+            
+            success = placeInTeam(currentPlayer, teams);
+            if(success)
+            {
+                unassignedPlayers--;
+            }
+            else
+            {
+                swap(listOfPlayers, unassignedPlayers-1, fillPlayers);
+                fillPlayers++;
+            }
+        }
+        else // At this point the rest of the players either have fill in their roles or are being autofilled.
+        {
+            var filledTeams = 0;
+            // If it crashes from out of bounds here it means there were more than 5*teams players in the playerList
+            while(!teams[filledTeams].fillTeam(currentPlayer))
+                filledTeams++;
+
+            unassignedPlayers--;
+        }
+    }
+}
+
+// Places the player in a team based on their primary and secondary roles. If they don't fit or they've selected fill, returns 0
+function placeInTeam(player, teamList)
+{
+    for(var i = 0; i < teamList.length; i++)
+    {
+        switch(player.rolePrimary){
+            case 'F':
+                return 0;
+            case 'T':
+                if(!teamList[i].top)
+                {
+                    teamList[i].top = player;
+                    return 1;
+                }
+                break;
+            case 'J':
+                if(!teamList[i].jung)
+                {
+                    teamList[i].jung = player;
+                    return 1;
+                }
+                break;
+            case 'M':
+                if(!teamList[i].mid)
+                {
+                    teamList[i].mid = player;
+                    return 1;
+                }
+                break;
+            case 'A':
+                if(!teamList[i].adc)
+                {
+                    teamList[i].adc = player;
+                    return 1;
+                }
+                break;
+            case 'S':
+                if(!teamList[i].supp)
+                {
+                    teamList[i].supp = player;
+                    return 1;
+                }
+                break;
+            default:
+                console.error("UNKNOWN ROLE DETECTED");
+                return -1;
+        }
+    }
+
+    // if we reach this point, the player's primary is already taken in all teams
+    for(var i = 0; i < teamList.length; i++)
+    {
+        switch(player.rolePrimary){
+            case 'F':
+                return 0;
+            case 'T':
+                if(!teamList[i].top)
+                {
+                    teamList[i].top = player;
+                    teamList.secondaries++;
+                    return 1;
+                }
+                break;
+            case 'J':
+                if(!teamList[i].jung)
+                {
+                    teamList[i].jung = player;
+                    teamList.secondaries++;
+                    return 1;
+                }
+                break;
+            case 'M':
+                if(!teamList[i].mid)
+                {
+                    teamList[i].mid = player;
+                    teamList.secondaries++;
+                    return 1;
+                }
+                break;
+            case 'A':
+                if(!teamList[i].adc)
+                {
+                    teamList[i].adc = player;
+                    teamList.secondaries++;
+                    return 1;
+                }
+                break;
+            case 'S':
+                if(!teamList[i].supp)
+                {
+                    teamList[i].supp = player;
+                    teamList.secondaries++;
+                    return 1;
+                }
+                break;
+            default:
+                console.error("UNKNOWN ROLE DETECTED");
+                return -1;
+
+        }
+    }
+    return 0;
 }
 
 /*  
@@ -301,7 +499,7 @@ function shuffle(list)
 
 function byMMR(a, b)
 {
-    return (a.mmr < b.mmr)?(1):(-1);
+    return (a.getMMR() < b.getMMR())?(1):(-1);
 }
 
 function byGamesMissed(a, b)
@@ -319,6 +517,13 @@ function playerListContains(listToCheck, username)
                 contains = index;
         });
     return contains;
+}
+
+function swap(list, i, j)
+{
+    var temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
 }
 
 // -----------------------------------------END HELPERS---------------------------------------------------------
