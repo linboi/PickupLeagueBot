760375bf683d4514e1fef396bf708e80094283cd
Ciaran Ruane <ciruane@tcd.ie>
2019-09-30 17:28:05 +0100

    Add monday to game days

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 // -----CONSTANTS-----
 
 const VERSION = '1.1.1';
-const gameDays = [3]; // 0 is sunday, 1 is monday etc
+const gameDays = [1]; // 0 is sunday, 1 is monday etc
 const signUpTime = 20;
 const gameTimes = [45, 105]; // minutes from signup time to team announcement
 
